897c2471950b1723ee1bfbe1794a4c610cbf9517
Kevin Dryfuse <kevin@kevindryfuse.com>
2021-12-12 21:28:20 -0500

    Changes that I hope will make postgres work in Heroku

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1) or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
